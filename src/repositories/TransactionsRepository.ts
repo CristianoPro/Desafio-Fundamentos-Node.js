89d1b2e2d5db30bd55a9f40aa0eb04b3d8e37038
Cristiano S Oliveira <cristiano.so@hotmail.com>
2020-08-28 01:11:46 -0300

    feat: challenge completed

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,35 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let income = 0;
+    let outcome = 0;
+    this.transactions.forEach(trasaction => {
+      if (trasaction.type === 'income') {
+        income += trasaction.value;
+      } else if (trasaction.type === 'outcome') {
+        outcome += trasaction.value;
+      }
+    });
+    const total = income - outcome;
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
